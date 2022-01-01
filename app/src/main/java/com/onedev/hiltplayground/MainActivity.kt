102528579386438314da2dc7d3aa4f16c0842f0b
rwnhrmwn <rwnhermawan@gmail.com>
2022-01-01 07:58:19 +0700

    Field & Constructor Injection | @AndroidEntryPoint, @Inject

--- app/src/main/java/com/onedev/hiltplayground/MainActivity.kt
+++ app/src/main/java/com/onedev/hiltplayground/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.onedev.hiltplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // Field Injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing())
+        println(someClass.doSomeOtherThing())
+    }
+}
+
+class SomeClass
+@Inject
+constructor(private val someOtherClass: SomeOtherClass) {
+    fun doAThing(): String {
+        return "Look I did a Thing!"
+    }
+
+    fun doSomeOtherThing(): String {
+        return someOtherClass.doSomeOtherThing()
+    }
+}
+
+class SomeOtherClass
+@Inject
+constructor() {
+    fun doSomeOtherThing(): String {
+        return "Look I did a Other Thing!"
     }
 }
